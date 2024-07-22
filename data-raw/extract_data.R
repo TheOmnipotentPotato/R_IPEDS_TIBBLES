3e3435ecb13f4e240cac14ee23439a1bd93c1d5a
TheOmnipotentPotato <jacobnickodemus@Jacobs-MacBook-Pro.local>
2024-07-22 16:39:42 -0600

    extract_data script completed

--- data-raw/extract_data.R
+++ data-raw/extract_data.R
@@ -1,16 +1,53 @@
 library(Hmisc)
-
+library(purrr)
+library(tidyverse)
+library(logger)
 setwd("/Users/julianickodemus/Coding/R/ipedsData")
 
+log_path <- "/Users/julianickodemus/Coding/R/ipedsData/ipedsTablesRPKG/ipedstables/data-raw/db_log.log"
 databases <- list.files(pattern = ".*\\.accdb")
+log_appender(appender_file(log_path))
 
-exract_database <- function(path) {
+
+
+
+
+
+extract_database <- function(path) {
     year_str <- gsub(pattern = "^[A-Z]*", "-", path) |>
         gsub(pattern = "\\.accdb", replacement = "-") |>
         substr(4, 5)
     db_out <- Hmisc::mdb.get(path, stringsAsFactors = FALSE)
+    log_info("Extracted {path}")
     list(year = year_str, data = db_out)
 }
 
+get_table <- function(table_name, database) {
+    tb <- database[[table_name]] |> tibble()
+    list(data = tb, name = table_name)
+}
 
-exract_database(databases[1])
+
+
+get_table_names <- function(database, year) {
+    database[[paste0("Tables", year)]][["TableName"]]
+}
+
+
+write_table_to_Rda <- function(table, name) {
+    assign(paste0(name), table)
+    save(list = name, file = paste0(name, ".Rda"))
+    log_info("Created Rda for {name}")
+}
+
+database_to_Rda <- function(database) {
+    names <- get_table_names(database$data, database$year)
+    tables <- names |> purrr::map(\(name) get_table(name, database$data))
+    setwd("/Users/julianickodemus/Coding/R/ipedsData/ipedsTablesRPKG/ipedstables/data/")
+    tables |> purrr::map(\(table) write_table_to_Rda(table$data, table$name))
+}
+
+# databases |> purrr::map(\(database) print(database))
+databases |>
+    purrr::map(\(database) extract_database(database)) |>
+    purrr::map(\(database) database_to_Rda(database))
